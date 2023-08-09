9feb4cc653088341e081a23655398c38ff312947
贺春旸 <chunyang_he@139.com>
2023-08-09 23:35:13 +0800

    Add files via upload

--- src/sql_count_value.py
+++ src/sql_count_value.py
@@ -3,25 +3,33 @@ import pymysql
 def count_column_value(table_name, field_name, mysql_settings, sample_size):
     with pymysql.connect(**mysql_settings) as conn:
         with conn.cursor() as cursor:
-            # 构造SQL查询语句
-            sql = f"""
-                SELECT COUNT(*) as count
-                FROM (
-                    SELECT {field_name}
-                    FROM {table_name}
-                    LIMIT 100000
-                ) AS subquery
-                GROUP BY {field_name}
-                HAVING COUNT(*) >= {sample_size}/2;
+            
             """
-
+            在这个查询中，使用了CASE语句来判断数据行数是否小于100000。如果数据行数小于100000，则使用
+            (SELECT COUNT(*) FROM {table_name}) / 2
+            作为阈值，即表的实际大小除以2；否则使用 {sample_size} / 2 作为阈值。
+            """
+            
+            sql = f"""
+            SELECT COUNT(*) as count
+            FROM (
+                SELECT {field_name}
+                FROM {table_name}
+                LIMIT {sample_size}
+            ) AS subquery
+            GROUP BY {field_name}
+            HAVING COUNT(*) >= CASE WHEN (SELECT COUNT(*) FROM {table_name} LIMIT {sample_size}) < {sample_size} 
+            THEN (SELECT COUNT(*) FROM {table_name}) / 2 ELSE {sample_size} / 2 END;
+            """
+            #print(sql)
             cursor.execute(sql)
 
             results = cursor.fetchall()
 
     if results:
-        # 如果有超过5万条的重复数据
+        # 如果有超过半数的重复数据
         return results
     else:
         return False
+
 
